67a20ea4a1c043b198a2eb01ea5cde1fc4359714
WuNL <wunailiang@gmail.com>
2014-11-17 14:52:46 +0800

    rm ugv

--- src/ugv/velodyne1_to_pc/build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
-
-export CATKIN_TEST_RESULTS_DIR="/program/catkin_ws/src/ugv/velodyne1_to_pc/build/test_results"
-export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
