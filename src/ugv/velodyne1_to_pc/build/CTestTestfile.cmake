8d10ae428d0e0fa6f234bce3333e2e5b7ec6174f
WuNL <wunailiang@gmail.com>
2014-11-17 14:51:12 +0800

    daad

--- src/ugv/velodyne1_to_pc/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /program/catkin_ws/src/ugv/velodyne1_to_pc
-# Build directory: /program/catkin_ws/src/ugv/velodyne1_to_pc/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
